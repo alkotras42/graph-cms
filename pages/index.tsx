a2a9825e11076b480a87b79ec340d173af88a96a
alkotras42 <kirill.stogniy@mail.ru>
2022-04-05 17:57:45 +0300

    Fetch data from GraphqlCMS

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,9 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Categories, PostCard, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  {
-    title: 'React Testing',
-    excerpt: 'Learn React Testing',
-  },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
-]
-
-const Home: NextPage = () => {
+const Home: NextPage = ({posts}) => {
   return (
     <div className="container mx-auto mb-8 px-10 ">
       <Head>
@@ -27,13 +20,23 @@ const Home: NextPage = () => {
 
         <div className="col-span-1 lg:col-span-4">
           <div className="relative top-8 lg:sticky">
-            <PostWidget/>
-            <Categories/>
+            <PostWidget />
+            <Categories />
           </div>
         </div>
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
 
 export default Home
